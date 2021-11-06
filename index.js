569bff5a593a60905948c977ebae3eb9f4f6d03c
UnknownX2007 <dinhkhoigaming@gmail.com>
2021-11-07 06:57:24 +0700

    main

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function getAB(){  
     var a = 0;
     var b = 0;
-    a = document.getElementById("a").value;
-    b = document.getElementById("b").value;  
+    a = Number(document.getElementById("a").value);
+    b = Number(document.getElementById("b").value);  
     var c = a+b;
     document.write(c);  
     }  
