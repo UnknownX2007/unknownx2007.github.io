cdb11f60c6bad7f3136bcf9bbf2f04866eff71ae
UnknownX2007 <dinhkhoigaming@gmail.com>
2021-11-28 11:59:04 +0700

    main

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function getmk(){  
     var a = 0;
     a = Number(document.getElementById("a").value);
-    if (a === "UnknownX2007") { 
+    if (a == "123456") { 
     document.write("Mã định danh: 083207005260");
     }  
 }  
