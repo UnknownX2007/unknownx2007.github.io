a04efbe80387d01bada9f4236b76d9e3fea8b22e
UnknownX2007 <dinhkhoigaming@gmail.com>
2021-11-29 00:58:48 +0700

    main

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function getAB(){  
     var a = 0;
     a = document.getElementById("a").value;
-    if (a == "KaliLinux2007") {
+    if (a == "Developed by VoDinhKhoi") {
     document.write("Mã định danh: 083207005260"); 
     }
 }  
