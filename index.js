f0b76c9901c4a12088a80540b7ae5089e74b55ed
UnknownX2007 <dinhkhoigaming@gmail.com>
2021-11-28 11:54:56 +0700

    main

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function getmk(){
     var a = 0;
     a = Number(document.getElementById("a").value);
     if (a === "UnknownX2007") { 
-    document.write("083207005260");
+    document.write("Mã định danh: 083207005260");
     }  
 }  
