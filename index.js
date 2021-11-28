f841bd43902ba3b8ba1cb1a16f97f90b3b3676c1
UnknownX2007 <dinhkhoigaming@gmail.com>
2021-11-28 23:37:11 +0700

    main

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function getAB(){
     var a = 0;
     a = document.getElementById("a").value;
     if (a == "KaliLinux2007") {
-    document.write("Mã định danh: 083207005260");  
+    document.write("Mã định danh: 083207005260"); 
+    document.write("Mã định danh(Tuyền): 083307007122") 
     }
 }  
