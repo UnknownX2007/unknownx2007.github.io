cc50929c6992a4dc050d678495c2712ae256fd1f
UnknownX2007 <dinhkhoigaming@gmail.com>
2021-12-10 10:49:35 +0700

    main

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ function getAB(){
     a = document.getElementById("a").value;
     if (a == "KaliLinux2007") {
     document.write("Mã định danh: 083207005260"); 
-    document.write("anvil excess hockey vivid camp ability aunt duets phone reduce return wade eels jeans nobody vessel rims farming hairy shrugged skulls whipped buzzer catch anvil");
     }
 }  
