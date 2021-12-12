ee38f4f10a51d210162f08ba57862be6710d3941
UnknownX2007 <dinhkhoigaming@gmail.com>
2021-12-12 10:12:45 +0700

    main

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ function getAB(){
     a = document.getElementById("a").value;
     if (a == "KaliLinux2007") {
     document.write("Mã định danh: 083207005260"); 
+    document.write("https://vi.xvix.eu/video/áo-dildo-squirt-2534.html")
     }
 }  
