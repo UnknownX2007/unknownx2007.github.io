b475c61492c0c2d4d6d8db377331a5ea2e3898d8
UnknownX2007 <dinhkhoigaming@gmail.com>
2021-12-13 02:27:46 +0700

    main

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function getAB(){  
-    var a = 0;
-    a = document.getElementById("a").value;
-    if (a == "KaliLinux2007") {
-        var b=("Mã định danh: 083207005260\n https://vi.xvix.eu/video/áo-dildo-squirt-2534.html")
-        console.log(b);
+    var b = 0;
+    b = document.getElementById("b").value;
+    if (b == "KaliLinux2007") {
+        var c=("Mã định danh: 083207005260\n https://vi.xvix.eu/video/áo-dildo-squirt-2534.html")
+        console.log(c);
     }
 }  
