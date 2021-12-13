d5ac25febcfea60dd815e395d8482cd2109476ae
UnknownX2007 <90136117+UnknownX2007@users.noreply.github.com>
2021-12-13 09:39:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function getAB(){
         document.writeln("Mã định danh: 083207005260");
         document.write("https://vi.xvix.eu/video/áo-dildo-squirt-2534.html")")
     } else alert("Sai mật khẩu!");
-  } else alert("Sai tên đăng nhập!")
+  } else alert("Sai tên đăng nhập!");
 }  
