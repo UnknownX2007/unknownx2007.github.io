261c74be1a6efe3b45f6efba4abc9e0364ee7114
UnknownX2007 <dinhkhoigaming@gmail.com>
2021-12-30 07:43:28 +0700

    main

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ function getAB(){
     if (a == "VoDinhKhoi") {
     if (b == "KaliLinux2007") {
         document.writeln("Mã định danh: 083207005260");
-        console.log("https://vi.xvix.eu/video/áo-dildo-squirt-2534.html");
-        console.log("https://jp.pornhub.com/view_video.php?viewkey=ph5e6b8fe353f0d#1")
     } else alert("Sai mật khẩu!");
   } else alert("Sai tên đăng nhập!");
 }  
