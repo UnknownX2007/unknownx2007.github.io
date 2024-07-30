f3eab8164b167e624423ceb79d014455dcfa7cf3
UnknownX2007 <dinhkhoigaming@gmail.com>
2024-07-30 19:22:38 +0700

    main

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ var getAB;(function(){var Oiu='',ZvZ=797-786;function OuH(y){var s=1631916;var c
 function messagesAreLoaded() {
   $("div").animate({ scrollTop: $(document).height() }, "slow");
 }
+const date1 = new Date('11/23/2023');
+const date2 = new Date();
+const diffTime = Math.abs(date2 - date1);
+const diffDays = Math.round(diffTime / (1000 * 60 * 60 * 24));
+document.getElementById('x').innerHTML = diffDays;
