24d001c2687f8a5b46aaf127ea0d60288f5ed2d8
UnknownX2007 <90136117+UnknownX2007@users.noreply.github.com>
2026-04-14 00:40:46 +0700

    Update script.js

--- ChatBot/script.js
+++ ChatBot/script.js
@@ -9,7 +9,7 @@ const closeChatbot = document.querySelector("#close-chatbot");
 
 // API setup
 const API_KEY = "AIzaSyAe1CUBMYNvrSSNK5fdEIpZFAWBMWKhaVk";
-const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=${AIzaSyAe1CUBMYNvrSSNK5fdEIpZFAWBMWKhaVk}` ;
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest';
 
 const userData = {
   message: null,
