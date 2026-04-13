c390a08cafcc5f43c6d1fd1f4ac5da17e5cc690e
UnknownX2007 <90136117+UnknownX2007@users.noreply.github.com>
2026-04-14 00:35:26 +0700

    Update script.js

--- ChatBot/script.js
+++ ChatBot/script.js
@@ -8,8 +8,8 @@ const chatbotToggler = document.querySelector("#chatbot-toggler");
 const closeChatbot = document.querySelector("#close-chatbot");
 
 // API setup
-const API_KEY = "#####";
-const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
+const API_KEY = "AIzaSyAe1CUBMYNvrSSNK5fdEIpZFAWBMWKhaVk";
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=${AIzaSyAe1CUBMYNvrSSNK5fdEIpZFAWBMWKhaVk}` ;
 
 const userData = {
   message: null,
